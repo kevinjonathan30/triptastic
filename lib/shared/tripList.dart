3574ee8e013b7262afb3bf952d2a1a5877308848
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-05-15 20:48:06 +0700

    Added animated list

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -9,12 +9,14 @@ class TripList extends StatefulWidget {
 
 class _TripListState extends State<TripList> {
   List<Widget> _tripTiles = [];
-  final GlobalKey _listKey = GlobalKey();
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   @override
   void initState() {
     super.initState();
-    _addTrips();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _addTrips();
+    });
   }
 
   void _addTrips() {
@@ -29,6 +31,7 @@ class _TripListState extends State<TripList> {
 
     _trips.forEach((Trip trip) {
       _tripTiles.add(_buildTile(trip));
+      _listKey.currentState.insertItem(_tripTiles.length - 1);
     });
   }
 
@@ -65,13 +68,18 @@ class _TripListState extends State<TripList> {
     );
   }
 
+  Tween<Offset> _offset = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return AnimatedList(
         key: _listKey,
-        itemCount: _tripTiles.length,
-        itemBuilder: (context, index) {
-          return _tripTiles[index];
+        initialItemCount: _tripTiles.length,
+        itemBuilder: (context, index, animation) {
+          return SlideTransition(
+            child: _tripTiles[index],
+            position: animation.drive(_offset),
+          );
         });
   }
 }
