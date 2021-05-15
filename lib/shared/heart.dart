df1a74c148a24751ba670abe481365528c7c3b01
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-05-15 20:19:42 +0700

    Added pulse animation on heart button in detail

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -8,17 +8,31 @@ class Heart extends StatefulWidget {
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   bool isFav = false;
   AnimationController _controller;
-  Animation _colorAnimation;
+  Animation<Color> _colorAnimation;
+  Animation<double> _sizeAnimation;
 
   @override
   void initState() {
     super.initState();
 
     _controller =
-        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+        AnimationController(duration: Duration(milliseconds: 300), vsync: this);
 
     _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
         .animate(_controller);
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 25, end: 30),
+          weight: 50
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 25),
+          weight: 50
+        ),
+      ]
+    ).animate(_controller);
 
     _controller.addListener(() {
       print(_controller.value);
@@ -54,7 +68,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: _colorAnimation.value,
-            size: 25,
+            size: _sizeAnimation.value,
           ),
           onPressed: () {
             isFav ? _controller.reverse() : _controller.forward();
